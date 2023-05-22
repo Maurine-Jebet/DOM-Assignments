a777ad86dc132f24ead8f87f73e4a8036a18c729
Maurine-Jebet <maurinejebett@gmai.com>
2023-05-22 19:48:54 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,51 @@
+//Change document background color to
+//silver
+
+document.getElementById("en").style.backgroundColor="silver";
+
+
+
+
+
+//Change the font color for h1 title tag to
+//green
+document.getElementById("title").style.color="green";
+
+
+
+
+
+
+
+//Change the font case for h3 title tags to
+//uppercase
+document.getElementById("h3").style.textTransform="toUpperCase"
+
+
+
+
+
+
+//Add one more fruit to the fruits list
+let fruits = document.createElement("li");
+fruits.innerHTML = "Avocado"
+document.getElementById("fruList").appendChild(fruits)
+
+
+
+
+
+
+
+
+
+
+
+
+//Add one more vegetable to the vegetables
+//list
+let vegetables= document.createElement('li');
+vegetables.innerHTML="kales"
+document.getElementById("vegList").appendChild(vegetables);
+
+
